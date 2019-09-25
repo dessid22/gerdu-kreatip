c92792d2b41f4331a568fd173c92fb676c526c53
dessid22 <dessid@yandex.ru>
2019-09-25 03:40:19 +0300

    Хедер+адаптив

--- app/js/common.js
+++ app/js/common.js
@@ -1,34 +1,6 @@
 $( document ).ready(function(){
 
-	//Проверка Magnific PopUp
-	$(".open-popup-link").magnificPopup({
-	  type:'inline'
-	});
-
-	//Проверка Animate
-	$("h1").animated("fadeInRight", "fadeInLeft");
-
-	// Проверка Waypoints
-	$("h2").waypoint(function() {
-		$("h2").css({'transform': 'scale(2)', 'text-align':'center'});
-	});
-
-	// Настройки .slider
-	$(".slider").owlCarousel({
-		items: 1,
-		margin: 10,
-		nav: true,
-		navText: "",
-		loop: true,
-		smartSpeed: 700,
-		fluidSpeed: 700,
-		navSpeed: 700,
-		dotsSpeed: 700,
-		dragEndSpeed: 700
-	});
-
 	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
 	$("form").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
